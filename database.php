c082bf3c35066b9c5d4b24e54368c60a2846b690
Mario Herbers <mario_herbers@hotmail.com>
2011-12-27 22:46:44 +0100

    Database added a function to get array from the database resource

--- database.php
+++ database.php
@@ -3,19 +3,62 @@
 /**
  * @author Mario Herbers
  */
-class database {
+class Database {
     
+
+    
+    public function __construct()
+    {
+        $database = 'gameplug';
+        mysql_connect('localhost', 'root', '');
+        mysql_select_db($database);
+    }
     
     /**
      *  
      * @param int $type
-     * @param String $query 
+     * @param array Array(table-> $query 
      */
-    public function query($type,$query)
+    public  function query($sql,$vars = array(),$insert = false)
+    {      
+       $query = $sql;    
+       if(is_scalar($vars))
+            {
+                    $vars = array($vars);
+            }
+
+            if(count($vars) > 0)
+            {
+                    $args = array($sql);
+
+                    foreach($vars as $v)
+                    {
+                            $args[] = mysql_real_escape_string($v);
+                    }
+
+                    $query = call_user_func_array('sprintf', $args);
+            }
+           $result= mysql_query($query);
+           //echo $query;
+           return (!$insert)?$result:mysql_insert_id();
+    }
+    
+    
+    public function resultArray($result)
     {
-        
+        $array = array();
+                            if(is_resource($result))
+                    {    
+                         if(mysql_num_rows($result)!=0) 
+                         {     while($row=mysql_fetch_assoc($result))
+                             {
+                                $array[]=$row;
+                             }
+                         }
+                    }
+        return $array;
     }
     
 }
 
-?>
+
